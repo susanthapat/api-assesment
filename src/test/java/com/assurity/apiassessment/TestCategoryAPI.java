6fab6acfff346d962402cd71d2f1de28954f219d
Susantha Pathirana <susanthap@techprovint.com>
2022-07-12 17:42:17 +0530

    removed api endpoint and organized response validation

--- src/test/java/com/assurity/apiassessment/TestCategoryAPI.java
+++ src/test/java/com/assurity/apiassessment/TestCategoryAPI.java
@@ -6,18 +6,16 @@ import org.testng.annotations.Test;
 import com.jayway.restassured.response.ValidatableResponse;
 
 @Test
-public class TestCategoryAPI {
+public class TestCategoryAPI extends BaseTest {
 
 	public void validateCategory() {
-		String apiRestEndPoint = "https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json";
 
-		ValidatableResponse response = given().queryParam("catalogue", "false").when().get(apiRestEndPoint).then().log()
-				.all().statusCode(200);
-
-		response.assertThat().body("Name", equalTo("Carbon credits"));
-		response.assertThat().body("CanRelist", equalTo(true));
-		response.assertThat().body("Promotions.findAll{it.Name=='Gallery'}.Description[0]",
-				equalTo("Good position in category"));
+		ValidatableResponse response;
+		response = given().queryParam("catalogue", "false").when().get(apiRestEndPoint).then().log().all()
+				.statusCode(200);
+		response.assertThat().body("Name", equalTo("Carbon credits"))
+				.assertThat().body("CanRelist", equalTo(true))
+				.assertThat().body("Promotions.findAll{it.Name=='Gallery'}.Description[0]", equalTo("Good position in category"));
 
 	}
 }
